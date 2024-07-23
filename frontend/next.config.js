866eeca865974204d86405890e35b57bb7f26516
Adam Hockemeyer <hock2k5@gmail.com>
2024-07-23 16:48:06 -0400

    chore: Remove unused "util" dependency and update Azure Static Web Apps workflow

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
     output: "standalone",
 }
-
-module.exports = {
-    async redirects() {
-        return [
-            {
-                source: '/((?!.swa).*)<YOUR MATCHING RULE>',
-                destination: '<YOUR REDIRECT RULE>',
-                permanent: false,
-            },
-        ]
-    },
-};
