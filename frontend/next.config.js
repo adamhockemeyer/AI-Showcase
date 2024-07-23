8c0975feabeca128164913f24179e69c57201a1e
Adam Hockemeyer <hock2k5@gmail.com>
2024-07-23 15:56:22 -0400

    chore: Update Azure Static Web Apps workflow to add redirect rule for health page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
     output: "standalone",
 }
+
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+                destination: '<YOUR REDIRECT RULE>',
+                permanent: false,
+            },
+        ]
+    },
+};
