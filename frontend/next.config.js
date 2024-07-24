ea6dc38bc697b42a233e268eead727ec822ffb2a
Adam Hockemeyer <hock2k5@gmail.com>
2024-07-24 10:47:54 -0400

    chore: Update Azure Static Web Apps workflow to remove unused code and add redirect rule for health page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,28 @@
 module.exports = {
     output: "standalone",
 }
+
+// module.exports = {
+//     async redirects() {
+//         return [
+//             {
+//                 source: '/((?!.swa).*)/health.html',
+//                 destination: '/health.html',
+//                 permanent: false,
+//             },
+//         ]
+//     },
+// };
+
+// module.exports = {
+//     async rewrites() {
+//         return {
+//             beforeFiles: [
+//                 {
+//                     source: '/((?!.swa).*)/health.html',
+//                     destination: '/health.html',
+//                 }
+//             ]
+//         }
+//     },
+// };
